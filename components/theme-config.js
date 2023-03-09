c8d94650d131a49b32f8a8c2609c1ffea80f2b98
mooghoo <albertopablito8@gmail.com>
2023-03-09 13:41:47 -0700

    font face

--- components/theme-config.js
+++ components/theme-config.js
@@ -1,9 +1,15 @@
 import { createGlobalStyle} from "styled-components"
 export const GlobalStyles = createGlobalStyle`
-  body {
+@font-face {
+  font-family: 'BLMelody-Medium';
+  font-style: normal;
+  font-weight: 300;
+  src: url('./public/BLMelody-Medium.woff');
+}
+  * {
     background: #005640;
     color: #fff;
-    font-family: Open Sans, sans-serif;
+    font-family: "BLMelody-Medium";
     transition: all 0.50s linear;
   }
 `
